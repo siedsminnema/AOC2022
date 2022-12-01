812065129fcc9d71ef36c41a4792079467c406c0
Sieds Minnema <siedsminnema@gmail.com>
2022-12-01 20:32:27 +0100

    refactor

--- day_01.py
+++ day_01.py
@@ -1,13 +1,13 @@
 from input import get_input
 
 input = get_input(1)
-amount = 0
-totals = []
 
 def sorted_list(list):
+    amount = 0
+    totals = []
     for c in input:
         if c:
-            amount = int(c)
+            amount += int(c)
         else:
             totals.append(amount)
             amount = 0
