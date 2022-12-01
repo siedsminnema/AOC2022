e03c252c7901657c76ec5425850debf6b3e911e1
Sieds Minnema <siedsminnema@gmail.com>
2022-12-01 17:12:25 +0100

    solution day 1

--- day_01.py
+++ day_01.py
@@ -1,2 +1,19 @@
-from input import get_input
+with open('input_1.txt') as f:
+    data = f.read()
+    input_list = [i for i in data.split("\n")]
 
+amount = 0
+totals = []
+
+for c in input_list:
+    try:
+        amount += int(c)
+    except:
+        totals.append(amount)
+        amount = 0
+
+st = sorted(totals, reverse=True)
+result1 = (st[0])
+result2 = (st[0] + st[1] + st[2])
+
+print(result1, result2)
