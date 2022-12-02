8c1031a70f343c1a21c77763eeccf89db401d16f
Sieds Minnema <siedsminnema@gmail.com>
2022-12-02 13:31:12 +0100

    refactor

--- day_02.py
+++ day_02.py
@@ -1,35 +1,23 @@
 from input import get_input
 
 input = get_input(2)
-ts = 0
-d = {'X': 1, 'Y':2, 'Z':3}
-six = {'X': 'C', 'Y': 'A', 'Z': 'B'}
-three = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+score1, score2 = 0, 0
 
-part2 = {'X': 0, 'Y': 3, 'Z': 6}
-X = {'A': 3, 'B': 1, 'C': 2}
-Y = {'A': 1, 'B': 2, 'C': 3}
-Z = {'A': 2, 'B': 3, 'C': 1}
+# result dicts
+base_score = {'X': 1, 'Y':2, 'Z':3}
+result_score = dict.fromkeys(['C X', 'A Y', 'B Z'], 6) | \
+               dict.fromkeys(['A X', 'B Y', 'C Z'], 3) | \
+               dict.fromkeys(['B X', 'C Y', 'A Z'], 0)
+result_score2 = {'X': 0, 'Y':3, 'Z':6}
+base_score2 = dict.fromkeys(['A X', 'C Y', 'B Z'], 3) | \
+              dict.fromkeys(['C X', 'B Y', 'A Z'], 2) | \
+              dict.fromkeys(['B X', 'A Y', 'C Z'], 1)
 
-
+# calculate totals
 for g in input:
-    ts += d[g[2]]
-    if six[g[2]] == g[0]:
-        ts += 6
-    if three[g[2]] == g[0]:
-        ts += 3
-    else:
-        ts += 0
-print(ts)
+    score1 += base_score[g[2]]
+    score1 += result_score[g]
+    score2 += base_score2[g]
+    score2 += result_score2[g[2]]
 
-ts2 = 0
-for g in input:
-    ts2 += part2[g[2]]
-    if g[2] == 'X':
-        ts2 += X[g[0]]
-    if g[2] == 'Y':
-        ts2 += Y[g[0]]
-    if g[2] == 'Z':
-        ts2 += Z[g[0]]
-
-print (ts2)
+print (score1, score2)
