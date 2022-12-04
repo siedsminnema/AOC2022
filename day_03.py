b1e07350c0b8d2e77559fda76c7d9404caa3d0df
Sieds Minnema <siedsminnema@gmail.com>
2022-12-04 12:28:22 +0100

    day3 and day4

--- day_03.py
+++ day_03.py
@@ -1,1 +1,37 @@
 from input import get_input
+
+input = get_input(3)
+letters = []
+letters2 = []
+
+for s in input:
+    half = len(s)/2
+    s1, s2 = [], []
+    s1 = [j  for i,j in enumerate(s) if i < half]
+    s2 = [j  for i,j in enumerate(s) if i >= half]
+    for i in s1:
+        if i in s2:
+            letters.append(i)
+            break
+
+
+for s in range(0, len(input), 3):
+    s1, s2, s3 = input[s], input[s+1], input[s+2]
+    s1 = [i for i in s1]
+    s2 = [i for i in s2]
+    s3 = [i for i in s3]
+    for i in s1:
+        if i in s2 and i in s3:
+            letters2.append(i)
+            break
+
+def let_to_num(l):
+    numbers = []
+    for i in l:
+        if i.islower():
+            numbers.append(ord(i) - 96)
+        if i.isupper():
+            numbers.append(ord(i) - 64 + 26)
+    return numbers
+
+print(sum(let_to_num(letters)), sum(let_to_num(letters2)))
--- day_04.py
+++ day_04.py
@@ -1,1 +1,17 @@
 from input import get_input
+
+input = get_input(4)
+subsets = 0
+intersections = 0
+
+for i in input:
+    begin_1, end_1 = i.split(',')[0].split('-')
+    begin_2, end_2 = i.split(',')[1].split('-')
+    l1 = [a for a in range(int(begin_1), int(end_1)+1)]
+    l2 = [a for a in range(int(begin_2), int(end_2)+1)]
+    if set(l1).issubset(set(l2)) or set(l2).issubset(set(l1)):
+        subsets += 1
+    if len(set(l1).intersection(set(l2))) > 0:
+        intersections += 1
+
+print(subsets, intersections)
