6eea64bd7955b3c1de6fea6a8ca81502acd7dda9
Sieds Minnema <siedsminnema@gmail.com>
2022-12-05 09:58:06 +0100

    little bit better

--- day_05.py
+++ day_05.py
@@ -2,33 +2,46 @@ from input import get_input
 
 input = get_input(5)
 
-s = dict.fromkeys(range(1,10), '')
-print(s)
 
 # fill dict with strings
-for r in reversed(input):
-    if '[' in r:
-        print(r)
-        for row in range(1, 10):
-            b = -4 + 4 * row
-            e = -1 + 4 * row
-            if '[' in r[b:e]:
-                s[row] += r[b:e]
+def build_dict(input):
+    s = dict.fromkeys(range(1, 10), '')
+    for r in reversed(input):
+        if '[' in r:
+            for row in range(1, 10):
+                b = -4 + 4 * row
+                e = -1 + 4 * row
+                if '[' in r[b:e]:
+                    s[row] += r[b:e]
+    return s
 
-print(s)
 
+def move_blocks(s, input, CM9001=True):
+    for i, r in enumerate(input):
 
-for i, r in enumerate(input):
-    if i < 10:
-        continue
-    else:
-        amount = int((r.split(' from')[0].split('move ')[1]))
-        dep = int((r.split('from ')[1].split(' to')[0]))
-        arr = int((r.split(' to')[1]))
+        if i < 10:
+            continue
+        else:
+            amount = int((r.split(' from')[0].split('move ')[1]))
+            dep = int((r.split('from ')[1].split(' to')[0]))
+            arr = int((r.split(' to')[1]))
 
-        depstr = s[dep]
-        blocks = s[dep][-amount*3:]
-        # s[dep] = s[dep][]
-        s[arr] += blocks
-        print(r, s[dep], s[arr])
-        print(r, depstr, blocks)
+            if CM9001:
+                s[arr] += s[dep][-amount * 3:]
+                s[dep] = s[dep][:-amount * 3]
+
+            if not CM9001:
+                for blocks in range(0, amount):
+                    s[arr] += s[dep][-3:]
+                    s[dep] = s[dep][:-3]
+    return s
+
+s1, s2 = build_dict(input), build_dict(input)
+s1 = move_blocks(s1, input, CM9001=False)
+s2 = move_blocks(s2, input, CM9001=True)
+
+for item in s1:
+    print(s1[item][-2])
+
+for item in s2:
+    print("".join([s2[item][-2]]))
